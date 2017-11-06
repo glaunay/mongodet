b6551eac3f1dcc83cddb9db453994b97208412cf
Shang <hushangnong@gmail.com>
2017-11-06 12:52:47 +0100

    HTML fully get from server, favicon.ico

--- app.js
+++ app.js
@@ -12,54 +12,61 @@ var app = express();
 module.exports = app;
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/html/index.html')
+	res.sendFile(__dirname + '/html/index.html')
 })
 
 // catch 404 and forward to error handler
 /*
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 */
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 app.use('/script', express.static(__dirname + '/script'));
 app.use('/data', express.static(__dirname + '/data'));
 app.use('/favicon.ico', express.static(__dirname + '/images/favicon.ico'));
 app.use('/getHeader',function (req, res, next) {
-  res.send("<h1>Detergent Database CRUD Interface</h1>")
-  next();
+	res.send("<h1>Detergent Database CRUD Interface</h1>")
+	next();
+});
+app.use('/getTable',function (req, res, next) {
+	res.send("<table id='detable' class='display' cellspacing='0' width='100%''>"
+		+"<thead> <tr> <th>Category</th> <th>Name</th> <th>Volum</th>"
+		+"<th>Color</th> </tr> </thead> <tfoot> <tr> <th>Category</th>"
+		+"<th>Name</th> <th>Volum</th> <th>Color</th> </tr> </tfoot> </table>")
+	next();
 });
 
 //Partie BD
 /*
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/detest', function(err, db) {
-  if (err) {
-    throw err;
-  }
-  db.collection('det').find().toArray(function(err, result) {
-    if (err) {
-      throw err;
-    }
-    //console.log(result);
-  });
+	if (err) {
+		throw err;
+	}
+	db.collection('det').find().toArray(function(err, result) {
+		if (err) {
+			throw err;
+		}
+		//console.log(result);
+	});
 });
 */
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port 3000!')
 })
 /*
 var readJson=function(path) {
--- script/front.js
+++ script/front.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	// Set the header of page with title
+	// Get the header of page with title
 	var getHead = new XMLHttpRequest();
 	getHead.onreadystatechange=function() {
 	if (this.readyState == 4 && this.status == 200) {
@@ -9,48 +9,36 @@ $(document).ready(function() {
 	getHead.open("GET", "/getHeader", true);
 	getHead.send();
 	
-	$("#table").html("<table id='detable' class='display' cellspacing='0' width='100%''>"
-		+"<thead> <tr> <th>Category</th> <th>Name</th> <th>Volum</th>"
-		+"<th>Color</th> </tr> </thead> <tfoot> <tr> <th>Category</th>"
-		+"<th>Name</th> <th>Volum</th> <th>Color</th> </tr> </tfoot> </table>")
+	// Get the table field with data
+	var getTab = new XMLHttpRequest();
+	getTab.onreadystatechange=function() {
+	if (this.readyState == 4 && this.status == 200) {
+		$("#table").html(this.responseText);
+		// Initialize DataTable with options
+		$('#detable').DataTable( {
+			"processing": true,
+			"serverSide": false,
+			"ajax": "data/det.json",
+			"columns": [
+				{ "data": "category" },
+				{ "data": "name" },
+				{ "data": "vol" },
+				{ "data": "color" }
+			]
+		} );
+		}
+	};
+	getTab.open("GET", "/getTable", true);
+	getTab.send();
 
 	document.getElementById("text").innerHTML = "Ici y aura du texte explicatif";
-	/*
-	document.getElementById("table").innerHTML = "<table id='example' class='display' cellspacing='0' width='100%'> <thead> <tr>"
-		+"<th>Name</th> <th>Position</th> <th>Office</th> <th>Age</th> <th>Start date</th>"
-		+"<th>Salary</th> </tr> </thead> <tfoot> <tr> <th>Name</th> <th>Position</th>"
-		+"<th>Office</th> <th>Age</th> <th>Start date</th> <th>Salary</th> </tr> </tfoot>"
-		+"<tbody> <tr> <td>Tiger Nixon</td> <td>System Architect</td> <td>Edinburgh</td>"
-		+"<td>61</td> <td>2011/04/25</td> <td>$320,800</td> </tr> <tr> <td>Garrett"
-		+"Winters</td> <td>Accountant</td> <td>Tokyo</td> <td>63</td> <td>2011/07/25</td>"
-		+"<td>$170,750</td> </tr> <tr> <td>Ashton Cox</td> <td>Junior Technical"
-		+"Author</td> <td>San Francisco</td> <td>66</td> <td>2009/01/12</td>"
-		+"<td>$86,000</td> </tr> <tr> <td>Cedric Kelly</td> <td>Senior Javascript"
-		+"Developer</td> <td>Edinburgh</td> <td>22</td> <td>2012/03/29</td>"
-		+"<td>$433,060</td> </tr> <tr> <td>Airi Satou</td> <td>Accountant</td>"
-		+"<td>Tokyo</td> <td>33</td> <td>2008/11/28</td> <td>$162,700</td> </tr> <tr>"
-		+"<td>Brielle Williamson</td> <td>Integration Specialist</td> <td>New York</td>"
-		+"<td>61</td> <td>2012/12/02</td> <td>$372,000</td> </tr> </tbody> </table>";
-	*/
 
-	$('#detable').DataTable( {
-		"processing": true,
-		"serverSide": false,
-		"ajax": "data/det.json",
-		"columns": [
-			{ "data": "category" },
-			{ "data": "name" },
-			{ "data": "vol" },
-			{ "data": "color" }
-		]
-	} );
+	// button for jQuery testing
 	$("#btn1").click(function(){
 		$("#text").append(" <b>Appended text</b>.");
 	});
 	$("#btn2").click(function(){
 		$("#text").empty();
 	});
-	
-
 
 } );
