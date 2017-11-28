02c21323ff0b9f20cec88262486f531d44f66dde
SDS <sebastien.delolme-sabatier@etu.univ-lyon1.fr>
2017-11-28 22:16:38 +0100

    conv column names updated, res.json in data

--- bin/csv_to_json/conv.py
+++ bin/csv_to_json/conv.py
@@ -103,7 +103,7 @@ def write_header(f):
 	file_res.write("{\n    \"title\" : \"Detergent from converter\",\n    \"date\" : \"last modification date "+str(datetime.datetime.now())+"\",\n    \"author\" : \"CSV_TO_JSON_CONVERTER\",\n    \"data\" : {\n        ")
 
 
-columns = ["name","vol","color","complete_name","Molecular_formula","MM","CMC","Aggregation_number","Ref","PDB_file","detergent_image","SMILES"] # because columns are in french, we have to translate them to english
+columns = ["name","volume","color","complete_name","molecular_formula","mm","CMC","aggregation_number","ref","PDB_file","image","SMILES"] # because columns are in french, we have to translate them to english
 
 ######  Write the rest of the file  #####
 
