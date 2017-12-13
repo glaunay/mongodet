8c02fec9a510e3bbe5a6ecb66b504192e9424de4
SDS <sebastien.delolme-sabatier@etu.univ-lyon1.fr>
2017-12-13 17:19:01 +0100

    correction of a minor problem

--- app.js
+++ app.js
@@ -255,7 +255,7 @@ app.post('/updateDet',function (req, res) {
 		to_update.Aggregation_number = Number(to_update.Aggregation_number)
 	}
 	*/
-  	let updatedet = mongo.modifyDet(db,to_delete._id);
+  	let updatedet = mongo.modifyDet(db,to_update._id);
   	updatedet.on('modifOK',function(msg,result){
       	//ici insertion, on a imposé un ordre
       	res.send({"status":msg[0],"data":msg[1]} ) 
